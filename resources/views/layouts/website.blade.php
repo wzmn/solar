a7cf5f5f8c7d951d6aeafad714d944f7c04f4b26
creativeswasan <creativeswasan@gmail.com>
2024-08-30 10:20:30 +0530

    Update website.blade.php

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -12,7 +12,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     <meta charset="UTF-8">
 <!-- Title Canonical and Description - Start -->
 <title>Enernew @hasSection('title') - @endif @yield('title') </title>
-<meta name="description" property="og:description" content="@yield('title')" />
+<meta name="description" property="og:description" content="@URL()" />
 <link rel="canonical" href="@yield('title')" />
 
 <!-- End -->
